c1f2c7484cd05074b5ffe4aae6a18eaf4ab847f1
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-07-02 15:40:58 +1000

    HttpEntityWithContentTypeToHttpEntityConverter 2of2

--- src/it/junit-test/src/test/java/test/JunitTest.java
+++ src/it/junit-test/src/test/java/test/JunitTest.java
@@ -5,6 +5,15 @@ import com.google.j2cl.junit.apt.J2clTestInput;
 import org.junit.Assert;
 import org.junit.Test;
 
+import walkingkooka.convert.Converter;
+import walkingkooka.convert.ConverterContext;
+import walkingkooka.convert.ConverterContexts;
+import walkingkooka.convert.Converters;
+import walkingkooka.net.convert.NetConverters;
+import walkingkooka.net.header.HttpHeaderName;
+import walkingkooka.net.header.MediaType;
+import walkingkooka.net.http.HttpEntity;
+
 @J2clTestInput(JunitTest.class)
 public class JunitTest {
 
@@ -14,6 +23,36 @@ public class JunitTest {
         Assert.assertEquals(
                 1,
                 1
+        );
+    }
+
+    @Test
+    public void testConverter() {
+        final MediaType contentType = MediaType.ANY_TEXT;
+        final Converter<ConverterContext> converter = NetConverters.httpEntityContentType(contentType);
+        final String text = "BodyText123";
+
+        checkEquals(
+                text,
+                converter.to(
+                        String.class,
+                        Converters.hasTextToString()
+                ).convertOrFail(
+                        HttpEntity.EMPTY.addHeader(
+                                HttpHeaderName.CONTENT_TYPE,
+                                MediaType.TEXT_PLAIN
+                        ).setBodyText(text),
+                        String.class,
+                        ConverterContexts.fake()
+                )
+        );
+    }
+
+    private void checkEquals(final Object expected,
+                             final Object actual) {
+        Assert.assertEquals(
+                expected,
+                actual
         );
     }
 }
--- src/test/java/walkingkooka/net/convert/sample/Sample.java
+++ src/test/java/walkingkooka/net/convert/sample/Sample.java
@@ -1,6 +1,21 @@
-package test;
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
-import com.google.gwt.junit.client.GWTTestCase;
+package walkingkooka.net.convert.sample;
 
 import walkingkooka.convert.Converter;
 import walkingkooka.convert.ConverterContext;
@@ -10,19 +25,12 @@ import walkingkooka.net.convert.NetConverters;
 import walkingkooka.net.header.HttpHeaderName;
 import walkingkooka.net.header.MediaType;
 import walkingkooka.net.http.HttpEntity;
-@walkingkooka.j2cl.locale.LocaleAware
-public class TestGwtTest extends GWTTestCase {
+import walkingkooka.test.Testing;
 
-    @Override
-    public String getModuleName() {
-        return "test.Test";
-    }
+public class Sample implements Testing {
 
-    public void testAssertEquals() {
-        assertEquals(
-                1,
-                1
-        );
+    public static void main(final String[] args) {
+        new Sample().testConverter();
     }
 
     public void testConverter() {
@@ -43,14 +51,6 @@ public class TestGwtTest extends GWTTestCase {
                         String.class,
                         ConverterContexts.fake()
                 )
-        );
-    }
-
-    private void checkEquals(final Object expected,
-                             final Object actual) {
-        assertEquals(
-                expected,
-                actual
         );
     }
 }
